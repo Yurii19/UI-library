b2d2bf655de42090a2be2ddd9bdd2d9e5d32c2b4
Yurii <yurkomits@gmail.com>
2020-04-03 19:39:55 +0300

    carousel: done

---
+++ js/carousel.js
@@ -0,0 +1,74 @@
+window.onload = init;
+
+function init() {
+  // const screen = document.getElementById('carousel1');
+  const myimg = 'https://loremflickr.com/320/240';
+  //const slideWidth = 500;
+
+  // let nodeImg = document.createElement('img');
+  // nodeImg.src = myimg;
+  // nodeImg.className = 'virtualImg';
+  // nodeImg.width = '400';
+
+  // screen.append(nodeImg);
+  const config1 = ['https://loremflickr.com/320/240', 'https://loremflickr.com/320/240/brazil,rio', 'https://loremflickr.com/g/320/240/paris,girl/all'];
+  let id = 'carousel1';
+
+  drawSlider(id, config1)
+
+  // Carousel("carousel1", config1);
+  const buttonL = document.getElementById('button-left');
+  // const buttonR = document.getElementById('button-right');
+
+}
+
+function drawSlider(sliderAreaId, config1) {
+  const screen = document.getElementById(sliderAreaId);
+  const slideWidth = 400;
+  const sliderSize = config1.length;
+  const buttonL = document.getElementById('button-left');
+  const buttonR = document.getElementById('button-right');
+
+  for (let i = 0; i < sliderSize; i++) {
+    let nodeImg = document.createElement('img');
+    nodeImg.src = config1[i];
+    nodeImg.className = 'virtualImg';
+    nodeImg.width = slideWidth;
+    screen.append(nodeImg);
+  }
+
+  let currentPosition = 0;
+   showButton(currentPosition,buttonL, buttonR,sliderSize);
+
+  buttonL.addEventListener('click', () => {
+    if (currentPosition == sliderSize-1) {
+      return;
+    }
+    currentPosition += 1;
+    const disposition = slideWidth * currentPosition*-1;
+    screen.style.marginLeft = disposition.toString() + 'px';
+    showButton(currentPosition,buttonL, buttonR,sliderSize);
+  });
+  buttonR.addEventListener('click', () => {
+    if (currentPosition == 0) {
+      return;
+    }
+    currentPosition -= 1;
+    const disposition = slideWidth * currentPosition*-1;
+    screen.style.marginLeft = disposition.toString() + 'px';
+    showButton(currentPosition,buttonL, buttonR,sliderSize);
+  })
+}
+
+function showButton(position, bl, br, cap) {
+  if (position == 0) {
+    br.classList.add('button-hide');
+  } else {
+    br.classList.remove('button-hide');
+  }
+  if (position == cap-1) {
+    bl.classList.add('button-hide');
+  } else {
+    bl.classList.remove('button-hide');
+  }
+}
