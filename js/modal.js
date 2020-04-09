a4fc1cfaabbd6fd939d3329d4bf560d2694c0c95
Yurii <yurkomits@gmail.com>
2020-04-09 23:26:11 +0300

    gh-pages: modal now render static

--- js/modal.js
+++ js/modal.js
@@ -1,62 +1,18 @@
 
-let flagShow = false;
+ 
 let Modal = document.getElementById("modal-padding");
-
-let closeButton = document.createElement('button');
-closeButton.innerHTML = 'X';
-closeButton.className = 'close';
-
-let modalHeader = document.createElement('h4');
-modalHeader.innerHTML = 'Warning !';
-modalHeader.className = 'modal-header';
-
-let modalMessage = document.createElement('div');
-modalMessage.className = 'modalMes';
-
-let modalFooter = document.createElement('div');
-modalFooter.className = 'modal-footer';
-
-let acceptButton = document.createElement('button');
-acceptButton.innerHTML = 'Accept';
-acceptButton.className = 'footer-button';
-
-let rejectButton = document.createElement('button');
-rejectButton.innerHTML = 'Reject';
-rejectButton.className = 'footer-button';
-
-
-const modalSend = document.getElementById('send');
-
-modalSend.prepend(closeButton);
-closeButton.after(modalHeader);
-modalHeader.after(modalMessage);
-modalMessage.after(modalFooter);
-modalFooter.append(acceptButton, rejectButton);
-
-checkFlag(Modal, flagShow);
 
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('modal-trigger')) {
-    modalMessage.innerText = event.target.dataset.target;
-    flagShow = true;
-    checkFlag(Modal, flagShow);
+    let modId = event.target.dataset.target;
+    Modal.classList.toggle('hide-modal');
+    document.getElementById(modId).classList.toggle('hide-modal');
   }
 });
 
-closeButton.addEventListener('click', closeModal);
-acceptButton.addEventListener('click', closeModal);
-rejectButton.addEventListener('click', closeModal);
-
-function closeModal() {
-  flagShow = false;
-  checkFlag(Modal, flagShow);
-}
-
-function checkFlag(el, flag) {
-  console.log('checkFlag');
-  if (flag) {
-    el.classList.remove('hide-modal');
-  } else {
-    el.classList.add('hide-modal');
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('modal-close')) {
+    Modal.classList.toggle('hide-modal');
+    event.target.parentNode.classList.toggle('hide-modal');
   }
-}
+});
