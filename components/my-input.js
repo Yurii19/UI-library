43495271d8b210869bbfe27a1b0290ed44eacc9a
Yurii <yurkomits@gmail.com>
2020-05-02 13:56:30 +0300

    input: task#4 done

--- components/my-input.js
+++ components/my-input.js
@@ -1,33 +1,48 @@
 Vue.component('my-input', {
-    props: { type: String, title: String, placeholder: String, required: Boolean },
-    data: function () {
-      return {
-        userMessage: '',
-        nonFocused: false
+  props: { type: String, title: String, placeholder: String, required: Boolean, pattern: String, errormessage: String },
+  data: function () {
+    return {
+      userMessage: '',
+      nonValid: false
+    }
+  },
+  methods: {
+    inpVal: function () {
+      const regExp = new RegExp(this.pattern);
+      let res = regExp.exec(this.userMessage);
+      if (!res || this.userMessage === '') {
+        this.nonValid = true;
+      } else {
+        this.nonValid = false;
       }
     },
-    methods: {
-      inpVal: function () {
-        this.nonFocused = true;
-      },
-      passValue: function() {
-        this.$emit('inputvalue', {fieldname: this.title, fieldvalue: this.userMessage});
-      }
-    },
-    template: `
+    passValue: function () {
+      this.$emit('inputvalue', { fieldname: this.title, fieldvalue: this.userMessage });
+    }
+  },
+  computed: {
+    errorFlag: function () {
+      return this.required && this.nonValid;
+    }
+  },
+  template: `
     <div>
-      <span>{{title}}
-      <span v-if="required">*</span>
-      </span>
-      <input 
-        v-bind:class = "{ nonValid: userMessage == '' && required && nonFocused }"
-        @blur="inpVal" 
-        @input="passValue"
-        v-model="userMessage" 
-        :type="type" 
-        :placeholder="placeholder" 
-        :required="required">
+      <label class="componentinput">
+        <span>{{title}}
+          <span v-if="required">*</span>
+          <transition name="fade">
+            <span class="errInpMessage"  v-if="nonValid">{{errormessage}}</span>
+          </transition>
+        </span>
+        <input 
+          v-bind:class = "{ inputError: errorFlag }"
+          @blur="inpVal" 
+          @input="passValue"
+          v-model="userMessage" 
+          :type="type" 
+          :placeholder="placeholder" 
+          :required="required">
+        </label>
     </div>
     `
-  })
-  
+})
--- main.js
+++ main.js
@@ -3,14 +3,15 @@ var app = new Vue({
   el: '#app',
   data: {
     questions: [
-      { title: "Как тебя зовут?", placeholder: "Иван" },
+      { title: "Как тебя зовут?", placeholder: "Иван", required: true},
       { title: "Сколько тебе лет?", placeholder: "15", type: "number" },
-      { title: "Твой email", placeholder: "ivan@example.com", required: true }
+      { title: "Твой email", placeholder: "ivan@example.com", required: true, 
+      pattern: '^\\w+@\\w+$', errormessage: 'Input email like "my@email"' } ///^\w+@\w+$/
     ]
   },
   methods: {
     handleInputedValue: function (data) {
-      for (const key in data) {console.log(data[key]) }
+     // for (const key in data) {console.log(data[key]) }
     }
   }
 }) 
